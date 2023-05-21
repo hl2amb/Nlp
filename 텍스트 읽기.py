35b6df69147d6ae760082342610925e7d97dce77
hl2amb <66735522+hl2amb@users.noreply.github.com>
2023-05-21 10:11:04 +0900

    Add files via upload

---
+++ 텍스트 읽기.py
@@ -0,0 +1,38 @@
+# 텍스트 읽기
+
+# f = open('sampleText.txt', 'r')
+# data = f.read()             # 텍스트 전체 가져 오기
+# print(data)
+# f.close()
+
+# 텍스트 한 줄씩 가져오고 numbering 하기
+
+# f = open('sampleText.txt', 'r')
+# line_num = 1
+# line = f.readline()
+#
+# while line:
+#     # print('[%d] %s' %(line_num, line), end='')
+#     print('[{}]  {}'.format(line_num, line))
+#     line = f.readline()
+#     line_num += 1
+# f.close
+
+# readlines() 와 enumerate()를 이용해서 한줄씩 읽기
+
+# f = open('sampleText.txt', 'r')
+# lines = f.readlines()
+#
+# for line_num, line in enumerate(lines):
+#
+#     print('[{}]  {}'.format(line_num+1, line))
+#
+# f.close()
+
+
+# with open() 으로 텍스트 불러 오기 close() 할 필요가 없음
+
+with open('sampleText.txt', 'r') as f:
+    text = f.read()
+    print(text)
+
